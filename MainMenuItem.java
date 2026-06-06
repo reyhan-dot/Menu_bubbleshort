fc1598d16805146b3c98f80e9381cbd45b137cf5
reyhan-dot <reyhanalakmal@gmail.com>
2026-06-06 10:21:00 +0700

    Update MainMenuItem.java

--- MainMenuItem.java
+++ MainMenuItem.java
@@ -5,7 +5,7 @@
 package menuitem;
 
 
-import java.util.Scanner;
+
 
 /**
  *
@@ -18,20 +18,12 @@ public class MainMenuItem {
      * 
      */
     public static void main(String[] args){
-        // TODO code application logic here
-        Scanner sc = new Scanner(System.in);
-
-        Menuclass menu = new Menuclass();
-        Childclass_Hemat murah = new Childclass_Hemat();
-        Childclass_Favorit favorit = new Childclass_Favorit();
-        
-        int pilihan;
-        System.out.println("==== Menu ====");
-        menu.tampilkanMenu();
+       
+        //tampilkan menu
         
         do{
         
-            System.out.println("==========");
+        System.out.println("==========");
         System.out.println("Pilihan Type Menu:");
         //dari harga termurah
         System.out.println("1. Pilihan Hemat");
@@ -39,24 +31,16 @@ public class MainMenuItem {
         System.out.println("2. Pilihan Puas");
         System.out.println("3. exit");
         
-        pilihan = sc.nextInt();
+
         
         switch(pilihan){
             
             case 1:
-                System.out.println("Rekomendasi Menu Hemat");
-                murah.hemat();
-                break;
                 
             case 2:
-                System.out.println("Rekomendasi Menu Favorit");
-                favorit.favorit();
-                break;
                 
             case 3:
-                System.out.println("Program Selesai");
-                
-                break;
+
         }
         
         }while (pilihan !=3);
